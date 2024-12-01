564fb541856169708c369978ad25e97ae67481e3
dbanaszak1 <dominikbanaszak530@gmail.com>
2024-12-01 13:46:53 +0100

    biometric-auth

--- Components/SetPasswordScreen.tsx
+++ Components/SetPasswordScreen.tsx
@@ -1,20 +1,22 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, Alert, StyleSheet } from 'react-native';
 import { storePassword, storeNote } from '../app/storage';
-
 import { NavigationProp } from '@react-navigation/native';
+import store_key from '../app/biometric';
 
 const SetPasswordScreen = ({ navigation }: { navigation: NavigationProp<any> }) => {
   const [password, setPassword] = useState('');
 
   const handleSetPassword = async () => {
-    if (password.length < 6) {
-      Alert.alert('Error', 'Password must be at least 6 characters long.');
+    if (password.length < 10) {
+      Alert.alert('Error', 'Password must be at least 10 characters long.');
       return;
     }
     await storePassword(password);
     console.log('Password set successfully');
-    await storeNote('', password); // Save an empty note initially
+    await store_key.store_key(password);
+    console.log('Biometric key stored');
+    await storeNote('', password); // Save an empty note
     console.log('Empty note saved');
     Alert.alert('Success', 'Password set successfully.');
     navigation.navigate('Login');
